73c7d31bd49f95df5f1ceb5c62ea043aa17fd5f9
Caick Bertin <bertinviana@gmail.com>
2023-05-22 17:10:26 -0300

    service layer, component registration

--- src/main/java/com/devdbigode/webservice/repositories/UserRepository.java
+++ src/main/java/com/devdbigode/webservice/repositories/UserRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devdbigode.webservice.entites.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
+public interface UserRepository extends JpaRepository<User, Integer>{ 
 }
--- src/main/java/com/devdbigode/webservice/resources/UserResources.java
+++ src/main/java/com/devdbigode/webservice/resources/UserResources.java
@@ -1,19 +1,33 @@
 package com.devdbigode.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devdbigode.webservice.entites.User;
+import com.devdbigode.webservice.services.UserServices;
 
 @RestController
 @RequestMapping( value = "/users")
 public class UserResources {
+
+    @Autowired
+    private UserServices services; 
     
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User("AAAA", "@issoAi", "696969", "*******");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = services.findAll();  
+        return ResponseEntity.ok().body(list); 
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findByUser(@PathVariable Integer id){
+        User u = services.findById(id); 
         return ResponseEntity.ok().body(u); 
     }
 }
