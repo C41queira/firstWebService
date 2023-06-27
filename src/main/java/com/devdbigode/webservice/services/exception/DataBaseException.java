7997ad5f09893813c7d5a5e6678f3f0b53645114
Caick Bertin <bertinviana@gmail.com>
2023-06-27 14:24:43 -0300

    ExceptionHandling - Delete

---
+++ src/main/java/com/devdbigode/webservice/services/exception/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.devdbigode.webservice.services.exception;
+
+public class DataBaseException extends RuntimeException{
+    
+    public DataBaseException(String msg){
+        super(msg); 
+    }
+}
